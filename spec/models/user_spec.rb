0b3e8434518e1b60bd5b98197bd80482965c63a4
ataman <ataman@gmail.com>
2013-08-15 12:01:45 -0700

    Chapter 11 partial

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,15 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
   it { should be_valid }
 
@@ -157,6 +166,30 @@ describe User do
       it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
     end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    
+  end
+
+  describe "and unfollowing" do
+    before { @user.unfollow!(other_user) }
+
+    it { should_not be_following(other_user) }
+    its(:followed_users) { should_not include(other_user) }
   end
 
 end
